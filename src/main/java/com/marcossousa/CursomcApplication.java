3e5d50d82e4b0be2a5ada7be49b925d9943adc98
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-20 15:51:37 -0300

    Criando Operacao de Instanciacao

--- src/main/java/com/marcossousa/CursomcApplication.java
+++ src/main/java/com/marcossousa/CursomcApplication.java
@@ -1,14 +1,37 @@
 package com.marcossousa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcossousa.domain.Categoria;
+import com.marcossousa.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		
+	}
+	
+	
 
 }
