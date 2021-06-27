acef2538d579ea4c535069bf428b218b286fdd9a
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-27 17:43:11 -0300

    PAGINACAO COM PARAMETRO OPCIONAIS NA REQUISICAO

--- src/main/java/com/marcossousa/services/CategoriaService.java
+++ src/main/java/com/marcossousa/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.marcossousa.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcossousa.domain.Categoria;
@@ -44,5 +48,17 @@ public class CategoriaService {
 		}
 
 	}
+	
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
+	}
+
 
 }
