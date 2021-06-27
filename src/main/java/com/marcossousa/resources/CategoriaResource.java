309a8e9743a1106bfae6c8b8254630b1ec48cdc0
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-27 15:16:28 -0300

    Listando todas categorias com DTO

--- src/main/java/com/marcossousa/resources/CategoriaResource.java
+++ src/main/java/com/marcossousa/resources/CategoriaResource.java
@@ -4,6 +4,8 @@
 package com.marcossousa.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcossousa.domain.Categoria;
+import com.marcossousa.dto.CategoriaDTO;
 import com.marcossousa.services.CategoriaService;
 
 /**
@@ -56,6 +59,15 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+
+	}
+
 
 	
 }
--- src/main/java/com/marcossousa/services/CategoriaService.java
+++ src/main/java/com/marcossousa/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.marcossousa.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,11 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel Excluir um Categoria que possui produtos");
 		}
 
+	}
+
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
 	}
 
 }
