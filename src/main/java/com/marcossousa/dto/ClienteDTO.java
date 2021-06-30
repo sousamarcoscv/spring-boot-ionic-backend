b1b074748249464fffaa1a8a602753082b618e3b
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-06-30 18:40:53 -0300

    PUT, DELETE E GET para Cliente

---
+++ src/main/java/com/marcossousa/dto/ClienteDTO.java
@@ -0,0 +1,65 @@
+package com.marcossousa.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.marcossousa.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty (message = "Preenchimento Obrigatorio")
+	@Size(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	@Email(message = "E-mail invalido")
+	@NotEmpty (message = "Preenchimento Obrigatorio")
+	private String email;
+	
+	
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
