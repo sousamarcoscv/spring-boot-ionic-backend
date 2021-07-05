f0824395d589a56ae6d13f7975c0356d4d049b47
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-07-05 19:17:15 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/marcossousa/dto/ClienteNewDTO.java
+++ src/main/java/com/marcossousa/dto/ClienteNewDTO.java
@@ -2,21 +2,45 @@ package com.marcossousa.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.marcossousa.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Size(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Email(message = "Email invalido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cpfouCnpj;
+	
 	private Integer tipo;
 
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cep;
 
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 
---
+++ src/main/java/com/marcossousa/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.marcossousa.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.marcossousa.domain.enums.TipoCliente;
+import com.marcossousa.dto.ClienteNewDTO;
+import com.marcossousa.resources.exception.FieldMessage;
+import com.marcossousa.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfouCnpj())) {
+			list.add(new FieldMessage("cpfouCnpj","CPF Invalido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfouCnpj())) {
+			list.add(new FieldMessage("cpfouCnpj","CNPJ Invalido"));
+		}
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
