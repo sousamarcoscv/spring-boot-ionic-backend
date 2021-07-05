0c945435b45e03e3725ff91342546bc8bbe9c8ba
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-07-05 19:29:54 -0300

    Validacao customizada: email não repetido na insercao de Cliente

--- src/main/java/com/marcossousa/repositories/ClienteRepository.java
+++ src/main/java/com/marcossousa/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.marcossousa.repositories;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.marcossousa.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/marcossousa/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcossousa/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcossousa.domain.Cliente;
 import com.marcossousa.domain.enums.TipoCliente;
 import com.marcossousa.dto.ClienteNewDTO;
+import com.marcossousa.repositories.ClienteRepository;
 import com.marcossousa.resources.exception.FieldMessage;
 import com.marcossousa.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +37,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfouCnpj","CNPJ Invalido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email","E-mail já existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
