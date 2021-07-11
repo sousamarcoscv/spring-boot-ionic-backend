7cf171ca355f6a5269bba7a2780b7316ac1c4bfd
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-07-11 14:46:06 -0300

    Busca de Produtos por nome e Categorias

--- src/main/java/com/marcossousa/repositories/ProdutoRepository.java
+++ src/main/java/com/marcossousa/repositories/ProdutoRepository.java
@@ -1,12 +1,20 @@
 package com.marcossousa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.marcossousa.domain.Categoria;
 import com.marcossousa.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome,List<Categoria> categorias,Pageable pageRequest);
 	
 }
---
+++ src/main/java/com/marcossousa/resources/utils/URL.java
@@ -0,0 +1,28 @@
+package com.marcossousa.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+	
+	public static List<Integer> decoteintLis(String s) {
+		List<Integer> list = new ArrayList<>();
+		String[] vet = s.split(",");
+		for (int i = 0; i < vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}
+		return list;
+	}
+
+}
