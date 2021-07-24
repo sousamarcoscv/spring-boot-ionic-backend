3f8b8088658fb61338b8e94870447de3cfafb7d3
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-07-24 13:02:57 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/marcossousa/config/DevConfig.java
+++ src/main/java/com/marcossousa/config/DevConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.marcossousa.services.DBService;
+import com.marcossousa.services.EmailService;
+import com.marcossousa.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
