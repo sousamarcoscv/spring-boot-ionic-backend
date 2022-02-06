22b5a0c057534d0dd20d3054bbf00c7563819d8f
Marcos Sousa <sousa.marcoscv@gmail.com>
2022-02-05 22:17:12 -0500

    Implementando autenticacao e geracao de token JWT

---
+++ src/main/java/com/marcossousa/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.marcossousa.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.marcossousa.domain.Cliente;
+import com.marcossousa.repositories.ClienteRepository;
+import com.marcossousa.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfils());
+	}
+
+}
