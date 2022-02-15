d8adf9ef40bc48a00f4226d57e3d905d4372bc35
marcos <marcos@casa>
2022-02-14 22:22:46 -0300

    Implementando autorizacao

--- src/main/java/com/marcossousa/config/SecurityConfig.java
+++ src/main/java/com/marcossousa/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.marcossousa.security.JWTAuthenticationFilter;
+import com.marcossousa.security.JWTAuthorizationFilter;
 import com.marcossousa.security.JWTUtil;
 
 @Configuration
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
