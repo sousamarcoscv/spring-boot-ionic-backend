2f2e05c2921a1604f3831a9d5593aa1658c08ed9
marcos <marcos@casa>
2022-03-13 14:30:01 -0300

    Refresh token

--- src/main/java/com/marcossousa/config/DevConfig.java
+++ src/main/java/com/marcossousa/config/DevConfig.java
@@ -2,8 +2,6 @@ package com.marcossousa.config;
 
 import java.text.ParseException;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
